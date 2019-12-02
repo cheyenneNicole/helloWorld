fc0a4c45049a7e41630b3c13c34af69c813c2ad6
HardikShivhare2508 <hardik.shivhare@gmail.com>
2019-12-02 10:28:55 -0700

    Initial commit

---
+++ src/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class HelloWorld {
+
+    //type psmv and tab creating main method
+    public static void main(String[] args){
+        System.out.println("HELLO WORLD!!");
+    }
+
+}
